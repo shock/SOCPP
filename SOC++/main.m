f7f154e9d9cfe74936c5777eb8999175c1defafd
shock <github@wdoughty.net>
2016-01-04 15:29:12 -0600

    Initial Commit

---
+++ SOC++/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SOC++
+//
+//  Created by Bill Doughty on 1/4/16.
+//  Copyright © 2016 Bill Doughty. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
